0b6b0e82996a1c426f1524444223431123e69544
manta1130 <46486762+manta1130@users.noreply.github.com>
2019-07-02 07:28:09 +0900

    標準入力関数の実装
    細部の変更
    
    input_line_str,input_line_numの実装
    
    input_vectorの実装
    
    inputマクロの実装
    
    Update README.md
    
    説明文の追加
    
    Update Cargo.toml
    
    Update main.rs
    
    inputマクロ不使用時に警告が出るのを修正
    
    Revert "Update main.rs"
    動かなかったので取り消し
    
    This reverts commit d9b58a0c52a734450550bf67776fcacfd0b0e415.
    
    input_vector関数をinput_vector2d関数にリネーム
    
    input_vector関数の実装
    
    説明文の追記・修正
    
    str2vec関数の実装
    説明文の追記
    
    str2vec関数が未使用時に警告が出るのを修正
    
    input_vector2d関数の細部を変更
    
    Update README.md
    
    Update README.md
    
    input_vector_2d関数のコード量を削減
    
    定数の宣言位置を変更
    
    p関数の追加
    
    input_vectorの実装

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# AtCoder_Template_Rust
+# Competitive_Programming_Template_Rust
+AtCoderとかの競技プログラミングでRustでの標準入力を簡単にする関数とマクロです。
+
+## inputマクロ
+標準入力から一行読み込み数値に変換します。
+```Rust
+let a:i32;
+let b:i32;
+let c:f64;
+
+//標準入力に"4"が入力されると変数aに4が代入されます。
+input!(a);
+
+//標準入力に"2 4.3"が入力されると変数bに2,変数cに4.3が代入されます。
+input!(b,c);
+```
+
+## input_line_str関数
+標準入力から一行読み込んでStringに変換します。
+改行文字は除去します。
+```Rust
+let s = input_line_str();
+```
+
+## input_vector2d関数
+標準入力から指定した行数を読み込み二次元配列(Vec)に変換します。
+```Rust
+let a:Vec<Vec<isize>>;
+//10行読み込む
+a = input_vector2d(10);
+```
+
+## input_vector関数
+標準入力から一行読み込み配列(Vec)に変換します。
+```Rust
+let a:Vec<isize>;
+a = input_vector();
+```
+
+## str2vec関数
+文字列を配列(Vec<char>)に変換します。
+```Rust
+let a = str2vec("abc");
+//['a','b','c']
+
+let b = str2vec(&input_line_str());
+//標準入力に入力した文字列を配列に変換
+
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,99 @@
+use std::io;
+
+#[allow(unused_macros)]
+macro_rules! input {
+    ( $($x:expr ),*) => {
+        {
+            let temp_str = input_line_str();
+            let mut split_result_iter = temp_str.split_whitespace();
+                $(
+                let buf_split_result = split_result_iter.next();
+                let buf_split_result = buf_split_result.unwrap();
+                    ($x) = buf_split_result.parse().unwrap();
+                )*
+        }
+    };
+}
+
+#[allow(dead_code)]
+fn input_line_str() -> String {
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).unwrap();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn p<T>(t: T)
+where
+    T: std::fmt::Display,
+{
+    println!("{}", t);
+}
+
+const SPLIT_DELIMITER: char = ' ';
+#[allow(dead_code)]
+fn input_vector2d<T>(line: usize) -> Vec<Vec<T>>
+where
+    T: std::str::FromStr,
+{
+    let mut v: Vec<Vec<T>> = Vec::new();
+
+    for _ in 0..line {
+        let vec_line = input_vector();
+        v.push(vec_line);
+    }
+    v
+}
+
+#[allow(dead_code)]
+fn input_vector<T>() -> Vec<T>
+where
+    T: std::str::FromStr,
+{
+    let mut v: Vec<T> = Vec::new();
+
+    let s = input_line_str();
+    let split_result = s.split(SPLIT_DELIMITER);
+    for z in split_result {
+        let buf = match z.parse() {
+            Ok(r) => r,
+            Err(_) => panic!("Parse Error"),
+        };
+        v.push(buf);
+    }
+    v
+}
+
+#[allow(dead_code)]
+fn str2vec(s: &str) -> Vec<char> {
+    let mut v: Vec<char> = Vec::new();
+    for c in s.chars() {
+        v.push(c);
+    }
+    v
+}
+
+fn input_vector<T>(line: usize) -> Vec<Vec<T>>
+where
+    T: std::str::FromStr,
+{
+    let mut v: Vec<Vec<T>> = Vec::new();
+
+    for _ in 0..line {
+        let s = input_line_str();
+        let split_result = s.split_whitespace();
+        let mut vec_line: Vec<T> = Vec::new();
+        for z in split_result {
+            let buf = match z.parse() {
+                Ok(r) => r,
+                Err(_) => panic!("Parse Error"),
+            };
+            vec_line.push(buf);
+        }
+        v.push(vec_line);
+    }
+    v
+}
+
 fn main() {
-    println!("Hello, world!");
 }
