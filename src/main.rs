e732b5b423a16da67b1996f9d1a28a842ce95537
manta1130 <manta@mt1996.com>
2019-07-02 07:53:47 +0900

    input_line_str,input_line_numの実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
+
+fn input_line_str() -> String {
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).unwrap();
+    s.trim().to_string()
+}
+
+fn input_line_number<T>() -> T
+where
+    T: std::str::FromStr,
+{
+    let s = input_line_str();
+    match s.parse() {
+        Ok(r) => r,
+        Err(_) => panic!("Parse Error"),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    
 }
