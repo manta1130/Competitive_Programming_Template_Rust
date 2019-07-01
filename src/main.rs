9d23dd68936af7737565b56eb6115ec63adef563
manta1130 <manta@mt1996.com>
2019-07-02 08:13:14 +0900

    input_vectorの実装

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,28 @@ where
     }
 }
 
+fn input_vector<T>(line: usize) -> Vec<Vec<T>>
+where
+    T: std::str::FromStr,
+{
+    let mut v: Vec<Vec<T>> = Vec::new();
+
+    for _ in 0..line {
+        let s = input_line_str();
+        let split_result = s.split_whitespace();
+        let mut vec_line: Vec<T> = Vec::new();
+        for z in split_result {
+            let buf = match z.parse() {
+                Ok(r) => r,
+                Err(_) => panic!("Parse Error"),
+            };
+            vec_line.push(buf);
+        }
+        v.push(vec_line);
+    }
+    v
+}
+
 fn main() {
-    
+      
 }
