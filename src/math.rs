f0c960036919ad2d7d744d34f86800cdaf9bfb1e
manta1130 <manta@mt1996.com>
2019-08-29 22:32:07 +0900

    拡張ユーグリッドの互除法の実装

--- src/math.rs
+++ src/math.rs
@@ -3,6 +3,7 @@
 
 use std;
 use std::cmp;
+use std::default;
 use std::ops;
 
 ///最大公約数を求める。
@@ -27,6 +28,29 @@ where
         + Copy,
 {
     (a / gcd(a, b)) * b
+}
+
+///拡張ユーグリッドの互除法
+pub fn extgcd<T>(a: T, b: T, x: &mut T, y: &mut T) -> T
+where
+    T: ops::Add<Output = T>
+        + ops::SubAssign
+        + ops::Rem<Output = T>
+        + ops::Div<Output = T>
+        + ops::Mul<Output = T>
+        + cmp::PartialEq
+        + default::Default
+        + From<bool>
+        + Copy,
+{
+    if b == T::default() {
+        *y = T::default();
+        *x = T::from(true);
+        return a;
+    }
+    let d = extgcd(b, a % b, y, x);
+    *y -= a / b * *x;
+    return d;
 }
 
 //}
