1cf84e9baa7e338c472c1bb58c9813d9e8ffc289
manta1130 <mt1996@live.jp>
2020-02-17 13:30:52 +0000

    Dinic法を実装

--- src/flow.rs
+++ src/flow.rs
@@ -1,6 +1,8 @@
 //提出時は下と一番下のコメントアウトを外す。
 //mod flow {
 use std;
+use std::collections::VecDeque;
+
 type CapValue = u64;
 const INF: CapValue = 999999999;
 
@@ -9,6 +11,96 @@ struct Edge {
     to: usize,
     cap: CapValue,
     rev: usize,
+}
+
+///最大流を計算(Dinicのアルゴリズム)
+pub struct Dinic {
+    n: usize,
+    g: Vec<Vec<Edge>>,
+    iter: Vec<usize>,
+    level: Vec<isize>,
+}
+
+impl Dinic {
+    ///要素数nのグラフを構築する。
+    pub fn new(n: usize) -> Dinic {
+        let res = Dinic {
+            n: n,
+            g: vec![vec![]; n],
+            iter: vec![],
+            level: vec![],
+        };
+        res
+    }
+    fn bfs(&mut self, s: usize) {
+        self.level = vec![-1; self.n];
+        let mut que = VecDeque::new();
+        self.level[s] = 0;
+        que.push_back(s);
+        while !que.is_empty() {
+            let v = que.pop_front().unwrap();
+            for i in 0..self.g[v].len() {
+                let e = &self.g[v][i];
+                if e.cap > 0 && self.level[e.to] < 0 {
+                    self.level[e.to] = self.level[v] + 1;
+                    que.push_back(e.to);
+                }
+            }
+        }
+    }
+    fn dfs(&mut self, k: usize, t: usize, f: CapValue) -> CapValue {
+        if k == t {
+            return f;
+        }
+        for i in self.iter[k]..self.g[k].len() {
+            let e = self.g[k][i];
+            if e.cap > 0 && self.level[k] < self.level[e.to] {
+                let r = self.dfs(e.to, t, std::cmp::min(f, e.cap));
+                if r > 0 {
+                    self.g[k][i].cap -= r;
+                    self.g[e.to][e.rev].cap += r;
+                    return r;
+                }
+            }
+        }
+
+        0
+    }
+    ///最大フローを求める。
+    pub fn get_maxflow(&mut self, s: usize, t: usize) -> CapValue {
+        let mut res = 0;
+        loop {
+            self.bfs(s);
+            if self.level[t] < 0 {
+                return res;
+            }
+            self.iter = vec![0; self.n];
+            loop {
+                let f = self.dfs(s, t, INF);
+                if f == 0 {
+                    break;
+                }
+                res += f;
+            }
+        }
+    }
+    ///辺の追加
+    ///
+    /// f->tにvalueの容量を持つ辺を追加する。
+    pub fn add_edge(&mut self, f: usize, t: usize, value: CapValue) {
+        let idx_t = self.g[t].len();
+        let idx_f = self.g[f].len();
+        self.g[f].push(Edge {
+            to: t,
+            cap: value,
+            rev: idx_t,
+        });
+        self.g[t].push(Edge {
+            to: f,
+            cap: 0,
+            rev: idx_f,
+        });
+    }
 }
 
 ///最大流を計算(Ford-Fulkersonのアルゴリズム)
