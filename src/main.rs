8cf40621d0069988461b1e72edf6f115f5e6f05a
manta1130 <mt1996@live.jp>
2020-02-25 11:29:16 +0000

    proconioのインポート文を追記

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,19 @@
 extern crate cp_template;
 use cp_template::*;
 
+//  ____            _                  _                       _       _
+// |  _ \ _   _ ___| |_    ___ _ __   | |_ ___ _ __ ___  _ __ | | __ _| |_ ___
+// | |_) | | | / __| __|  / __| '_ \  | __/ _ \ '_ ` _ \| '_ \| |/ _` | __/ _ \
+// |  _ <| |_| \__ \ |_  | (__| |_) | | ||  __/ | | | | | |_) | | (_| | ||  __/
+// |_| \_\\__,_|___/\__|  \___| .__/___\__\___|_| |_| |_| .__/|_|\__,_|\__\___|
+//                            |_| |_____|               |_|
+
+//https://github.com/manta1130/Competitive_Programming_Template_Rust
+
 #[allow(unused_imports)]
-use input::*;
+use proconio::{
+    fastout, input,
+    marker::{Bytes, Chars, Isize1, Usize1},
+};
 
 fn main() {}
