7dffa9047c584397e9b67c34710c06739606eb9d
manta1130 <manta@mt1996.com>
2020-08-30 12:53:54 +0900

    遅延セグ木の実装

--- src/segtree.rs
+++ src/segtree.rs
@@ -70,4 +70,111 @@ where
     }
 }
 
+///汎用の遅延セグメント木
+pub struct SegTreeLazy<T, F, G, H> {
+    //n: usize,
+    v: Vec<T>,
+    lazy: Vec<T>,
+    init: T,
+    lazy_init: T,
+    lazy_convert: F,
+    lazy_propagation: G,
+    parent_generator: H,
+}
+
+impl<T, F, G, H> SegTreeLazy<T, F, G, H>
+where
+    T: PartialOrd + Clone + Copy,
+    F: Fn(T, T) -> T,
+    G: Fn(T, T) -> T,
+    H: Fn(T, T) -> T,
+{
+    ///遅延セグメント木を構築する。
+    ///
+    /// # Examples
+    /// ```ignore
+    ///　let mut segtree = SegTreeLazy::new(100, 0, 0, |a, b| a + b, |a, b| a + b, |a, b| a + b / 2);
+    /// //要素数100の区間加算・区間取得のセグ木を構築する。
+    /// ```
+    pub fn new(
+        n: usize,
+        init: T,
+        lazy_init: T,
+        parent_generator: H,
+        lazy_convert: F,
+        lazy_propagation: G,
+    ) -> SegTreeLazy<T, F, G, H> {
+        let mut size = 1;
+        while size < n {
+            size *= 2;
+        }
+        let v = vec![init; size * 2 - 1];
+        let lazy = vec![init; size * 2 - 1];
+        SegTreeLazy {
+            v: v,
+            lazy: lazy,
+            init: init,
+            lazy_init: lazy_init,
+            lazy_convert: lazy_convert,
+            lazy_propagation: lazy_propagation,
+            parent_generator: parent_generator,
+        }
+    }
+
+    ///[l,r)の要素をparent_generatorに基づいて計算する。
+    pub fn get(&mut self, l: usize, r: usize) -> T {
+        let vlen = self.v.len();
+        self._get(l, r, 0, 0, (vlen + 1) / 2)
+    }
+
+    fn _get(&mut self, l: usize, r: usize, now: usize, a: usize, b: usize) -> T {
+        let propagation = &self.lazy_propagation;
+        let convert = &self.lazy_convert;
+        if now < self.lazy.len() && self.lazy[now] != self.lazy_init {
+            if now * 2 + 2 < self.lazy.len() {
+                self.v[now * 2 + 1] = propagation(self.v[now * 2 + 1], self.lazy[now]);
+                self.v[now * 2 + 2] = propagation(self.v[now * 2 + 2], self.lazy[now]);
+            }
+            self.v[now] = convert(self.v[now], self.lazy[now]);
+            self.lazy[now] = self.lazy_init;
+        }
+        if a >= r || b <= l {
+            self.init
+        } else if l <= a && b <= r {
+            self.v[now]
+        } else {
+            let bufa = self._get(l, r, now * 2 + 1, a, (a + b) / 2);
+            let bufb = self._get(l, r, now * 2 + 2, (a + b) / 2, b);
+            let cond = &self.parent_generator;
+            cond(bufa, bufb)
+        }
+    }
+
+    ///任意の要素を書き換える。
+    pub fn update(&mut self, l: usize, r: usize, v: T) {
+        let vlen = self.v.len();
+        self._update(l, r, 0, 0, (vlen + 1) / 2, v);
+    }
+
+    fn _update(&mut self, l: usize, r: usize, now: usize, a: usize, b: usize, v: T) {
+        let propagation = &self.lazy_propagation;
+        let convert = &self.lazy_convert;
+        if now < self.lazy.len() && self.lazy[now] != self.lazy_init {
+            if now * 2 + 2 < self.lazy.len() {
+                self.v[now * 2 + 1] = propagation(self.v[now * 2 + 1], self.lazy[now]);
+                self.v[now * 2 + 2] = propagation(self.v[now * 2 + 2], self.lazy[now]);
+            }
+            self.v[now] = convert(self.v[now], self.lazy[now]);
+            self.lazy[now] = self.lazy_init;
+        }
+        if l <= a && b <= r {
+            self.lazy[now] = propagation(self.lazy[now], v);
+        } else if !(a >= r || b <= l) {
+            let propagation = &self.lazy_propagation;
+            let next_v = propagation(self.lazy_init, v);
+            self._update(l, r, now * 2 + 1, a, (a + b) / 2, next_v);
+            self._update(l, r, now * 2 + 2, (a + b) / 2, b, next_v);
+        }
+    }
+}
 //}
